170fafa358c6f8706840cb43a89600629d1c21da
santoshkyamanakeri <reachme.santoshkyamanakeri@gmail.com>
2021-09-13 16:58:28 +0530

    css-variables

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('dark-theme-btn').addEventListener('click', () => {
+    document.documentElement.style.setProperty('--background-color', '#333')
+})
+
+
+document.getElementById('light-theme-btn').addEventListener('click', () => {
+    document.documentElement.style.setProperty('--background-color', 'blue')
+})
